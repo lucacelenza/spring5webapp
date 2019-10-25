0b050edcc2a6f2dfbba37f25b39ceb13486a9b89
Luca Celenza <celenzaluca@gmail.com>
2019-10-25 20:33:36 +0200

    Updated bootstrap class with publisher entity

--- src/main/java/clsoft/springFramework/spring5WebApp/bootstrap/DevBootstrap.java
+++ src/main/java/clsoft/springFramework/spring5WebApp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package clsoft.springFramework.spring5WebApp.bootstrap;
 
 import clsoft.springFramework.spring5WebApp.models.Author;
 import clsoft.springFramework.spring5WebApp.models.Book;
+import clsoft.springFramework.spring5WebApp.models.Publisher;
 import clsoft.springFramework.spring5WebApp.repositories.AuthorRepository;
 import clsoft.springFramework.spring5WebApp.repositories.BookRepository;
+import clsoft.springFramework.spring5WebApp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -12,10 +14,12 @@ import org.springframework.stereotype.Component;
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -25,9 +29,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData() {
         Author uncleBob = new Author("Robert C.", "Martin");
-        Book cleanCode = new Book("Clean Code", "9780132350884", "Robert C. Martin");
+        Book cleanCode = new Book("Clean Code", "9780132350884");
 
+        Publisher prenticeHall = new Publisher("Prentice Hall", "Upper Saddle River, New Jersey, US");
+        publisherRepository.save(prenticeHall);
+
+        cleanCode.setPublisher(prenticeHall);
         uncleBob.getBooks().add(cleanCode);
+
         cleanCode.getAuthors().add(uncleBob);
 
         authorRepository.save(uncleBob);
@@ -36,7 +45,11 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
         Author dino = new Author("Dino", "Esposito");
         Author andrea = new Author("Andrea", "Saltarello");
 
-        Book dotNetEnterprise = new Book("Microsoft .NET: Architecting Applications for the Enterprise - Second Edition", "0735685355", "Microsoft Press");
+        Publisher microsoftPress = new Publisher("Microsoft Press", "N/A");
+        publisherRepository.save(microsoftPress);
+
+        Book dotNetEnterprise = new Book("Microsoft .NET: Architecting Applications for the Enterprise - Second Edition", "0735685355");
+        dotNetEnterprise.setPublisher(microsoftPress);
 
         dino.getBooks().add(dotNetEnterprise);
         authorRepository.save(dino);
